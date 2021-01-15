3fd2430ba70d918098860090ad0303619cc6ccf4
Lili Storm <storm.aubrey@gmail.com>
2021-01-15 18:42:19 +0100

    Added all buttons and logic for checking player turn

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func boardButtonPressed(_ sender: UIButton) {
+        // print(sender.currentTitle!)
+        print(sender.accessibilityLabel!)
+        if (sender.currentTitle != nil) {
+            return
+        }
+        
+        if(playerTurn == 1) {
+                sender.setTitle("X", for: .normal)
+        } else {
+            sender.setTitle("O", for: .normal)
+        }
+        processPressedButton()
+    }
+    
+    var playerTurn : Int = 1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func processPressedButton() {
+        print("It was player \(playerTurn).")
+        if(playerTurn == 1) {
+            playerTurn = 2
+        } else {
+            playerTurn = 1
+        }
+        
+        print("It is now player \(playerTurn).")
+    }
 
 }
 
